42f2cc7c7bba7cd0e9fa0b220afa8d3fae74b8a7
João Silva <js84523@gmail.com>
2022-08-26 18:33:58 +0100

    Dados em ficheiro json e Limpeza Geral do código
    - criação de ficheiro json com os dados sobre os pontos de interesse
    - limpeza geral do código e simplificação
    - teste inicial de realidade aumentada
    - melhoria do aspeto da versão browser, recorrendo, por exemplo, a introdução de carrossel de imagens e de navbar

--- RaulLinoApp/www/js/index.js
+++ RaulLinoApp/www/js/index.js
@@ -44,115 +44,67 @@ function onLoad() {
 function onBackKeyDown() {
 }
 
-var onGPSSuccess = function(position) {
-   /* alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');*/
+var onGPSSuccess = function (position) {
+    /* alert('Latitude: '          + position.coords.latitude          + '\n' +
+           'Longitude: '         + position.coords.longitude         + '\n' +
+           'Altitude: '          + position.coords.altitude          + '\n' +
+           'Accuracy: '          + position.coords.accuracy          + '\n' +
+           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+           'Heading: '           + position.coords.heading           + '\n' +
+           'Speed: '             + position.coords.speed             + '\n' +
+           'Timestamp: '         + position.timestamp                + '\n');*/
     gpsSucess = true;
-    gpsPosition = position.coords; 
+    gpsPosition = position.coords;
 
-   /* var mapclient = L1.mapclient('mapclient').setView([position.coords.latitude , position.coords.longitude ], 13);
-    L1.tileLayer1('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mapclient);
+    /* var mapclient = L1.mapclient('mapclient').setView([position.coords.latitude , position.coords.longitude ], 13);
+     L1.tileLayer1('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+     }).addTo(mapclient);
+ 
+      L1.marker1([position.coords.latitude , position.coords.longitude ]).addTo(mapclient)
+      .bindPopup('<strong> estou no ipt</strong>')
+      .openPopup();
+ 
+ */
 
-     L1.marker1([position.coords.latitude , position.coords.longitude ]).addTo(mapclient)
-     .bindPopup('<strong> estou no ipt</strong>')
-     .openPopup();
-
-*/
-
-    distancia = GPSDistance(position.coords.latitude, position.coords.longitude, abrantesLat, abrantesLong); 
+    distancia = GPSDistance(position.coords.latitude, position.coords.longitude, abrantesLat, abrantesLong);
     var map;
 
     if (distancia < 5000) {
-        alert("estou em abrantes");
-        map = L.map('map').setView([position.coords.latitude,  position.coords.longitude], 14);
-        L.marker([position.coords.latitude , position.coords.longitude ]).addTo(map)
-        .bindPopup('<strong> GPS</strong>')
-        .openPopup();
+        map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 14);
+        L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
+            .bindPopup('<strong> GPS</strong>')
+            .openPopup();
     } else {
-        alert("não estou em abrantes");
         map = L.map('map').setView([abrantesLat, abrantesLong], 14);
-        L.marker([abrantesLat , abrantesLong ]).addTo(map)
-        .bindPopup('<strong> Centro Abrantes</strong>')
-        .openPopup();
+        L.marker([abrantesLat, abrantesLong]).addTo(map)
+            .bindPopup('<strong> Centro Abrantes</strong>')
+            .openPopup();
     }
- 
+
     document.addEventListener("backbutton", onBackKeyDown, false);
 
     function onBackKeyDown() {
-    // Handle the back button
+        // Handle the back button
     }
- 
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-   
 
-    L.marker([39.456233, -8.189917 ]).addTo(map)
-    .bindPopup('<a onclick="avportagempaiol();">AV. Portagem/AV. Paiol</a>');
+    fetch("dados.json")
+        .then(response => response.json())
+        .then(json => {
+            var i = 0;
+            json.dados.forEach(element => {
+                L.marker([element.coordenadas[0], element.coordenadas[1]]).addTo(map)
+                    .bindPopup('<a onclick="carrega_pagina(' + i + ');">' + element.titulo + '</a>');
+                i++;
+            });
+        });
 
-    L.marker([39.461142, -8.199598]).addTo(map)
-    .bindPopup('<a onclick="rlcamoes();">Rua Luis de Camões</a>');
 
-    L.marker([39.452866, -8.248377 ]).addTo(map)
-    .bindPopup('<a onclick="rfnova();">Rua da Fonte Nova</a>');
-
-    L.marker([39.461466, -8.198790 ]).addTo(map)
-    .bindPopup('<a onclick="lgamrma();">Largo General Avelar Machado e Rua Montepio Abrantino</a>');
-
-    L.marker([39.462793, -8.196086 ]).addTo(map)
-    .bindPopup('<a onclick="rsp();">Rua de S. Pedro, nº 8</a>');
-
-    L.marker([39.463610, -8.201411 ]).addTo(map)
-    .bindPopup('<a onclick="rdah();">Rua D. Afonso Henriques, nº 25</a>');
-
-    L.marker([39.461228, -8.198713 ]).addTo(map)
-    .bindPopup('<a onclick="lgam();">Largo General Avelar Machado</a>');
-
-    L.marker([39.461644, -8.197661 ]).addTo(map)
-    .bindPopup('<a onclick="rtv();">Rua Tenente Valadim, nº 3 e 5</a>');
-
-    L.marker([39.462735, -8.196801]).addTo(map)
-    .bindPopup('<a onclick="ra();">Rua do Arcediago, nº 19</a>');
-
-    L.marker([39.463106, -8.197944 ]).addTo(map)
-    .bindPopup('<a onclick="rgmam();">Rua General Manuel António Mourato, nº 11</a>');
-
-    L.marker([39.463001, -8.198164 ]).addTo(map)
-    .bindPopup('<a onclick="rje();">Rua José Estevão, nº 20</a>');
-
-    L.marker([39.463001, -8.198164 ]).addTo(map)
-    .bindPopup('<a onclick="rje();">Rua José Estevão, nº 20 </a>');
-
-    L.marker([39.461810, -8.197334 ]).addTo(map)
-    .bindPopup('<a onclick="rsi();">Rua de Santa Isabel nº 28 e 30 </a>');
-
-    L.marker([39.469188, -8.188591 ]).addTo(map)
-    .bindPopup('<a onclick="rpt();">Alferrarede, Rua do Porto Taínho, nº 24 </a>');
-
-    L.marker([39.463790, -8.200921 ]).addTo(map)
-    .bindPopup('<a onclick="rah();">Rua Afonso Henriques nº 9 a 11 </a>');
-
-    L.marker([39.463567, -8.201586 ]).addTo(map)
-    .bindPopup('<a onclick="ah();">Afonso Henriques (ao lado do nº 25)</a>');
-
-    L.marker([39.463702, -8.201182 ]).addTo(map)
-    .bindPopup('<a onclick="rah2();">Rua Afonso Henriques nº 13 a 15</a>');
-
-    L.marker([39.464757, -8.197524 ]).addTo(map)
-    .bindPopup('<a onclick="r5o();">Rua 5 de outubro de 1910, nº 16 </a>');
-
-    L.marker([39.417387, -8.210650 ]).addTo(map)
-    .bindPopup('<a onclick="csmrt();">Cemitério de S. Miguel do Rio Torto, Carvalhal </a>');
-    
     // centra o map
     map.setView(new L.LatLng([abrantesLat, abrantesLong]), 5);
 
@@ -160,25 +112,69 @@ var onGPSSuccess = function(position) {
 };
 
 function onGPSError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+    alert('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
     gpsSucess = false;
 }
 
 function GPSDistance(lat1, lon1, lat2, lon2) {
     const R = 6371e3; // metres
-    const teta1 = lat1 * Math.PI/180; // φ, λ in radians
-    const teta2 = lat2 * Math.PI/180;
-    const deltat = (lat2-lat1) * Math.PI/180;
-    const deltae = (lon2-lon1) * Math.PI/180;
-    
-    const a = Math.sin(deltat/2) * Math.sin(deltat/2) +
-              Math.cos(teta1) * Math.cos(teta2) *
-              Math.sin(deltae/2) * Math.sin(deltae/2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    
+    const teta1 = lat1 * Math.PI / 180; // φ, λ in radians
+    const teta2 = lat2 * Math.PI / 180;
+    const deltat = (lat2 - lat1) * Math.PI / 180;
+    const deltae = (lon2 - lon1) * Math.PI / 180;
+
+    const a = Math.sin(deltat / 2) * Math.sin(deltat / 2) +
+        Math.cos(teta1) * Math.cos(teta2) *
+        Math.sin(deltae / 2) * Math.sin(deltae / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
     const d = R * c; // in metres
 
     return d;
 
+}
+
+function carrega_pagina(id) {
+
+    fetch("dados.json")
+        .then(response => response.json())
+        .then(json => {
+
+            var carr =
+                '<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">' +
+                '<div class="carousel-inner">';
+            json.dados[id].imagens.forEach(element => {
+                carr += '<center><div class="carousel-item active" ><img  style="max-width:1000px; max-height:800px;" src="' + element + '" class="d-block w-100" ></div></center>'
+            });
+            carr += '</div>' +
+                '<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"' +
+                'data-bs-slide="prev">' +
+                '<span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
+                '<span class="visually-hidden">Previous</span>' +
+                '</button>' +
+                '<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"' +
+                'data-bs-slide="next">' +
+                '<span class="carousel-control-next-icon" aria-hidden="true"></span>' +
+                '<span class="visually-hidden">Next</span>' +
+                '</button>' +
+                '</div>';
+
+            document.getElementById("carImag").innerHTML = carr;
+
+            document.getElementById("pagina_titulo").textContent = json.dados[id].titulo;
+            document.getElementById("pagina_ano").textContent = json.dados[id].ano;
+            document.getElementById("pagina_localizacao").textContent = json.dados[id].localizacao;
+            document.getElementById("pagina_tipologia").textContent = json.dados[id].tipologia;
+            document.getElementById("pagina_informacao").textContent = json.dados[id].info;
+        });
+
+
+    document.getElementById("map").style.display = "none";
+    document.getElementById("pagina").style.display = "block";
+}
+
+function descarrega_pagina() {
+    document.getElementById("map").style.display = "block";
+    document.getElementById("pagina").style.display = "none";
 }
